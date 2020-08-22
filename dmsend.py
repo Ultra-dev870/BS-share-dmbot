4e4fca1d37798da52c8579ee39d6a6ff52bfaba2
Ultra-dev870 <70057915+Ultra-dev870@users.noreply.github.com>
2020-08-22 19:45:29 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('모든문의 Ultra#2679')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -28,11 +28,11 @@ async def on_message(message):
                     #메시지 관리권한이 있을시 사용가능
                     if message.author.guild_permissions.manage_messages:
                         embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at)
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text=f"discord.gg/★★서버초대코드★★")
+                        embed.add_field(name="크라운전문 울트라몰에서 전달한 메세지입니다.", value=msg, inline=True)
+                        embed.set_footer(text=f"discord.gg/haPCfm6")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzQ2Njc2MDUwODI1NzA3NTQy.X0DyQw.TYRHYV4R28rLiinfxRfV027cnW0')
